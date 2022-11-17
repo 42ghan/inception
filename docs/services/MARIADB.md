7feb55fc37b2cc1f2e4e1a28665349461829c0d7
Daniel Gyoungmin Han <ghan@student.42seoul.kr>
2022-11-17 21:00:41 +0900

    :sparkles: feat : mariadb dockerfile, entrypoint script, config draft

--- docs/services/MARIADB.md
+++ docs/services/MARIADB.md
@@ -3,6 +3,8 @@
 
 ## Setting Up & Start Server
 
+## Allowing Remote Client Access
+
 ## Create User & Prepare Tables
 
 ## Restarting MariaDB When Crashed
@@ -13,3 +15,4 @@
 - https://wiki.alpinelinux.org/wiki/MariaDB
 - https://mariadb.com/kb/en/running-mysqld-as-root/
 - https://mariadb.com/kb/en/what-to-do-if-mariadb-doesnt-start/
+- https://mariadb.com/kb/en/configuring-mariadb-for-remote-client-access/  
---
+++ srcs/services/mariadb/db_entrypoint.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env sh
+
+# Exit shell script when any command fails
+set -e
+
+# Install system table according to configuration provided by /etc/my.cnf
+# && Grant permission to access mysql data to the group, mysql
+mariadb-install-db --user=mysql
+# && chmod 750 /var/lib/mysql/mysql
+
+# Execute mariadb server
+# Create
+mariadbd --user=mysql &
+
+mariadb-admin ping --wait=.5;
+
+mariadb -u root -e "CREATE USER inception@172.18.0.3;" \
+&& mariadb -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'inception'@'172.18.0.3' IDENTIFIED BY 'ghandb';"
