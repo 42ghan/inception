eae52f6d1470441fed030ced18e7aed558a83148
Daniel Gyoungmin Han <ghan@student.42seoul.kr>
2022-11-18 18:34:24 +0900

    :sparkles: feat : docker-compose.yml compatible mariadb dockerfile draft

--- srcs/services/mariadb/db_entrypoint.sh
+++ srcs/services/mariadb/db_entrypoint.sh
@@ -14,5 +14,6 @@ mariadbd --user=mysql &
 
 mariadb-admin ping --wait=.5;
 
-mariadb -u root -e "CREATE USER inception@172.18.0.3;" \
+mariadb -u root -e "DROP DATABASE IF EXISTS test;" \
+&& mariadb -u root -e "CREATE USER inception@172.18.0.3;" \
 && mariadb -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'inception'@'172.18.0.3' IDENTIFIED BY 'ghandb';"
--- vm_setup.sh
+++ vm_setup.sh
@@ -46,8 +46,8 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-
-apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+apt-get update \
+&&apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 # bridge utils 설치
 apt-get install bridge-utils -y
@@ -67,6 +67,6 @@ go version
 pidtree --version
 brctl --version
 
-echo '{"features":{"buildkit":true}}' > /etc/docker/daemon.json
-
 service docker start
+
+echo '{"features":{"buildkit":true}}' > /etc/docker/daemon.json
