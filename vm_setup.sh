2f51a40c02cd9eae54e2e334dd97a5dd984beb0c
Gyoungmin Han <ghan@student.42seoul.kr>
2022-11-21 21:53:10 +0900

    :hammer: chore : update vm_setup.sh

--- vm_setup.sh
+++ vm_setup.sh
@@ -40,7 +40,7 @@ apt-get install \
     gnupg \
     lsb-release -y
 
-mkdir -p /etc/apt/keyrings && curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+mkdir -p /etc/apt/keyrings && curl -4fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
