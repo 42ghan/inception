d1175cda86fc8b0cd453c7caf76e8e9b19ece3d8
Daniel Gyoungmin Han <ghan@student.42seoul.kr>
2022-11-29 19:58:42 +0900

    :sparkles: feat : run wp-cli using su-exec

--- srcs/services/wordpress/wp_entrypoint.sh
+++ srcs/services/wordpress/wp_entrypoint.sh
@@ -3,19 +3,21 @@
 # Exit shell script when any command fails
 set -e
 
+alias su-wp="su-exec www-data wp"
+
 if [[ ! -f /var/www/ghan.42.fr/index.php ]]; then
 	cd /var/www \
-	&& wp core download --path=ghan.42.fr --version=latest \
+	&& su-wp core download --path=ghan.42.fr --version=latest \
 	&& cd ghan.42.fr \
-	&& wp config create --dbhost=$WP_DB_HOST --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_DB_PASSWD 
+	&& su-wp config create --dbhost=$WP_DB_HOST --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_DB_PASSWD
 	
 	# Wait until maraidb server is ready
 	mariadb-admin ping --user=$WP_DB_USER -p$WP_DB_PASSWD --host=$WP_DB_HOST --wait=.5 --connect-timeout=10
 	
-	wp db create \
-	&& wp core install --url=https://ghan.42.fr/ --title="ghan's inception" --admin_user=$WP_ADMIN_USER \
+	su-wp db create \
+	&& su-wp core install --url=https://ghan.42.fr/ --title=Inception --admin_user=$WP_ADMIN_USER \
 		--admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_ADMIN_EMAIL --skip-email \
-	&& wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWD --role=author
+	&& su-wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWD --role=author
 fi
 
 # Execute php-fpm
