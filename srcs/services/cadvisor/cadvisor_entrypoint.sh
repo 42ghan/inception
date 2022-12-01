884fe84602a558ac3fb7b9c2d66d504eaad062cc
Daniel Gyoungmin Han <ghan@student.42seoul.kr>
2022-12-01 22:03:49 +0900

    :sparkles: feat : cadvisor

---
+++ srcs/services/cadvisor/cadvisor_entrypoint.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env sh
+
+# Execute cadvisor
+echo -e '\n\nDreams feel real while we’re in them. It’s only when we wake up that we realize something was actually strange.\n\nCADVISOR IS UP!\n\n'
+exec tini -- cadvisor
