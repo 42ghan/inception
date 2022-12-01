2081e7ccb8d9649e007d6d1a9fbe4b581003302e
Daniel Gyoungmin Han <ghan@student.42seoul.kr>
2022-12-01 22:04:41 +0900

    :sparkles: feat add bonus services to existing system

--- srcs/services/adminer/adminer_entrypoint.sh
+++ srcs/services/adminer/adminer_entrypoint.sh
@@ -4,8 +4,6 @@ set -e
 
 curl -LO https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php \
 && mv adminer-4.8.1-mysql-en.php /var/www/adminer/index.php
-# && chown -R www-data:www-data /var/www/adminer \
-# && chown -R www-data:www-data /var/log/php7
 
 # Execute php-fpm
 echo -e '\n\n"The dreamer can always remember the genesis of the idea."\nADMINER IS READY!\n\n'
