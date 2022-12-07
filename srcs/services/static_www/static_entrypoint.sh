b5e6dfcdfac461b2d9557205974330e295657244
Daniel Han <danhaniyo@gmail.com>
2022-12-07 16:39:46 +0900

    :bug: fix : rm inception directory in static_www

--- srcs/services/static_www/static_entrypoint.sh
+++ srcs/services/static_www/static_entrypoint.sh
@@ -8,7 +8,8 @@ if [[ ! -f /var/www/static/CONTAINER.html ]]; then
   mkdir -p /var/www/static/services
   cd inception/docs
   find ./ -name "*.md" -type f -exec sh -c 'pandoc "${0}" -f markdown -t html -o "/var/www/static/${0%.md}.html"' {} \; \
-  && mv ./assets /var/www/static/assets
+  && mv ./assets /var/www/static/assets \
+  && rm -rf /root/inception
 fi
 
 echo -e '\n\n"True Inspiration Is Impossible To Fake."\n\nSTATIC WEBSITE IS READY!\n\n'
