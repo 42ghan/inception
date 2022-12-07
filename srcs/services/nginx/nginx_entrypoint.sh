0923d11f56160f0cdd89bbbd09a9acd03d18894e
Daniel Han <danhaniyo@gmail.com>
2022-12-07 18:22:00 +0900

    :sparkles: feat : custom CA cert and signing

--- srcs/services/nginx/nginx_entrypoint.sh
+++ srcs/services/nginx/nginx_entrypoint.sh
@@ -4,10 +4,16 @@ set -e
 
 # Creating and TLS certificate and dhparam
 mkdir -p /srv/www/ssl \
-&& openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
--keyout /srv/www/ssl/ghan.42.fr.key -out /srv/www/ssl/ghan.42.fr.crt \
--subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Education/CN=ghan.42.fr/emailAddress=$NGINX_USER_EMAIL" > /dev/null 2>&1 \
-&& openssl dhparam -out /srv/www/ssl/dhparam.pem 2048 > /dev/null 2>&1
+&& cd /srv/www/ssl
+
+openssl req -x509 -newkey rsa:4096 -days 365 -keyout ca-key.pem -out ca-cert.pem \
+-nodes -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Education/CN=ghan.42.fr/emailAddress=$NGINX_USER_EMAIL" > /dev/null 2>&1
+
+openssl req -newkey rsa:4096 -keyout ghan.42.fr-key.pem -out server-req.pem --nodes -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Education/CN=ghan.42.fr/emailAddress=$NGINX_USER_EMAIL" > /dev/null 2>&1
+
+openssl x509 -req -in server-req.pem -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out ghan.42.fr-cert.pem > /dev/null 2>&1
+
+openssl dhparam -out /srv/www/ssl/dhparam.pem 2048 > /dev/null 2>&1
 
 # Test NGINX
 nginx -t
