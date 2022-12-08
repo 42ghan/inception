81ef0498f1dd8a536081a3c53167ee9a8567027a
Daniel Gyoungmin Han <ghan@student.42seoul.kr>
2022-12-08 23:44:26 +0900

    :bug: fix : fix static_www error

--- srcs/services/static_www/static_entrypoint.sh
+++ srcs/services/static_www/static_entrypoint.sh
@@ -2,9 +2,8 @@
 
 set -e
 
-git clone https://github.com/42ghan/inception
-
 if [[ ! -f /var/www/static/CONTAINER.html ]]; then
+  git clone https://github.com/42ghan/inception
   mkdir -p /var/www/static/services
   cd inception/docs
   find ./ -name "*.md" -type f -exec sh -c 'pandoc "${0}" -f markdown -t html -o "/var/www/static/${0%.md}.html"' {} \; \
